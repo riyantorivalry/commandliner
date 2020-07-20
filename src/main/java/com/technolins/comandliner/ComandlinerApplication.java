9012a48821f3424fb95cb11db16497011e445d11
riyanto <riyanto.rivalry@gmail.com>
2020-07-20 16:51:10 +0700

    initialize spring and maven

---
+++ src/main/java/com/technolins/comandliner/ComandlinerApplication.java
@@ -0,0 +1,13 @@
+package com.technolins.comandliner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandlinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandlinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technolins/comandliner/ComandlinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technolins.comandliner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandlinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
